bea67db9cccc59bd598f4e532d16b180894f67bc
mytoandeptrai <mytoandn@gmail.com>
2021-10-14 12:59:02 +0700

    done server

--- controllers/authCtrl.js
+++ controllers/authCtrl.js
@@ -103,13 +103,14 @@ const authCtrl = {
   generateAccessToken: async (req, res) => {
     try {
       const rf_token = req.cookies.refreshtoken;
-      if (!rf_token) return res.status(400).json({ msg: "Please login now." });
+      if (!rf_token)
+        return res.status(400).json({ msg: "Please login now !." });
 
       jwt.verify(
         rf_token,
         process.env.REFRESH_TOKEN_SECRET,
         async (err, result) => {
-          if (err) return res.status(400).json({ msg: "Please login now." });
+          if (err) return res.status(400).json({ msg: "Please login now !." });
 
           const user = await Users.findById(result.id)
             .select("-password")
